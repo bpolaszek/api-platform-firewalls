87ef82112311dbaded072d036a1735789fe1a288
Beno!t POLASZEK <bpolaszek@gmail.com>
2019-05-23 15:16:32 +0200

    Sample entity

---
+++ src/Entity/Order.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\OrderRepository")
+ * @ORM\Table("`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Customer")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $customer;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $product;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numberOfCalls = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): self
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getProduct(): ?string
+    {
+        return $this->product;
+    }
+
+    public function setProduct(string $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getNumberOfCalls(): ?int
+    {
+        return $this->numberOfCalls;
+    }
+
+    public function setNumberOfCalls(int $numberOfCalls): self
+    {
+        $this->numberOfCalls = $numberOfCalls;
+
+        return $this;
+    }
+}
