9381f4e3ed1022ee69c9b03da7a3153403eb2915
Beno!t POLASZEK <bpolaszek@gmail.com>
2019-05-23 16:33:34 +0200

    Display authenticated API responses

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,28 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+import AxiosFactory from 'axios';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+const dataset = document.getElementById('content').dataset;
+
+(async () => {
+
+    const axios = AxiosFactory.create({
+        headers: {
+            Authorization: `Bearer ${dataset.jwt}`,
+        }
+    });
+
+
+    const orders = await axios.get(dataset.entrypoint)
+        .then(response => response.data.order)
+        .then(uri => axios.get(uri))
+        .then(response => response.data)
+    ;
+
+    console.log(orders);
+
+})();
